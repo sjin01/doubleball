4b36bf2e62e868b4b35f13dd784f1c9e0aeba21f
guanshj <xiao_wen202129@163.com>
2014-07-23 16:23:09 +0800

    分析雏形

--- doubleball-web/src/main/java/com/guanshj/controller/analyze/AnalyzeController.java
+++ doubleball-web/src/main/java/com/guanshj/controller/analyze/AnalyzeController.java
@@ -2,8 +2,10 @@ package com.guanshj.controller.analyze;
 
 import com.guanshj.controller.BaseController;
 import com.guanshj.enums.BallType;
+import com.guanshj.framework.util.AnalyzeUtil;
 import com.guanshj.model.DoubleBall;
 import com.guanshj.services.DoubleBallService;
+import com.guanshj.vo.DoubleBallDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -112,6 +114,88 @@ public class AnalyzeController extends BaseController{
     @RequestMapping(value = "currentAnalyze")
     public ModelAndView currentAnalyze ( Integer period) throws Exception{
         ModelAndView mv = new ModelAndView("analyze/currentAnalyze");
+
+        // 本期号码：
+        DoubleBall po = new DoubleBall();
+        po.setPeriod(period);
+        List<DoubleBall> list = doubleBallService.selectByEntity(po);
+
+        List<Integer> currentRed = new ArrayList<Integer>();   // 六个红球
+        Integer currentBlue = 0;  // 蓝球
+
+        for(DoubleBall item : list){
+            if(item.getType() == BallType.BLUE.getCode()){
+                // 蓝
+                currentBlue = item.getValue();
+            }else{
+                // 红
+                currentRed.add(item.getValue());
+            }
+        }
+        mv.addObject("currentPeriod" ,period);
+        mv.addObject("currentRed" ,currentRed);
+        mv.addObject("currentBlue" ,currentBlue);
+
+
+        // 上期号码：
+        period = period -1;
+        po = new DoubleBall();
+        po.setPeriod(period);
+        list = doubleBallService.selectByEntity(po);
+
+        List<Integer> prevRed = new ArrayList<Integer>();   // 六个红球
+        Integer prevBlue = 0;  // 蓝球
+
+        if(list!=null && !list.isEmpty()){  // 也许没有上一期的数据呢
+            for(DoubleBall item : list){
+                if(item.getType() == BallType.BLUE.getCode()){
+                    // 蓝
+                    prevBlue = item.getValue();
+                }else{
+                    // 红
+                    prevRed.add(item.getValue());
+                }
+            }
+            mv.addObject("prevPeriod" ,period);
+            mv.addObject("prevRed" ,prevRed);
+            mv.addObject("prevBlue" ,prevBlue);
+        }
+
+        // 上2期号码：
+        period = period -1;
+        po = new DoubleBall();
+        po.setPeriod(period);
+        list = doubleBallService.selectByEntity(po);
+
+        List<Integer> prev2Red = new ArrayList<Integer>();   // 六个红球
+        Integer prev2Blue = 0;  // 蓝球
+
+        if(list!=null && !list.isEmpty()){  // 也许没有上一期的数据呢
+            for(DoubleBall item : list){
+                if(item.getType() == BallType.BLUE.getCode()){
+                    // 蓝
+                    prev2Blue = item.getValue();
+                }else{
+                    // 红
+                    prev2Red.add(item.getValue());
+                }
+            }
+            mv.addObject("prev2Period" ,period);
+            mv.addObject("prev2Red" ,prev2Red);
+            mv.addObject("prev2Blue" ,prev2Blue);
+        }
+
+        // TODO 排除开始（本期分析）
+        List<Integer> sRedList = new ArrayList<Integer>();
+        List<Integer> sBlueList = new ArrayList<Integer>();
+
+        Integer sRed1 = AnalyzeUtil.sRed1(currentRed);
+        sRedList.add(sRed1);
+        mv.addObject("sRed1",sRed1);
+
+
+        mv.addObject("sRedList",sRedList);  // 全部杀得红球
+        mv.addObject("sBlueList",sBlueList); // 全部杀的蓝球
         return mv;
     }
 
